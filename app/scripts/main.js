ec8cc79c8ba8f6dd63dd3c80c727f16f67a8a6a9
kavinrajsi <sikavinraj@gmail.com>
2024-07-31 14:25:00 +0530

    ...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,13 @@
 
 AOS.init();
 
-const d = new Date();
-let text = d.toLocaleTimeString();
-document.getElementById("header__time").innerHTML = text;
-
-
 $(document).ready(function () {
-    $('#menu').click(function () {
+
+    const d = new Date();
+    let text = d.toLocaleTimeString();
+    $('.header__time').html(text);
+
+    $('#menu, #close').click(function () {
         // Toggle 'active' class on the navList
         $('.header__navlist').toggleClass('menu__active');
         // Toggle 'menu--noscroll' class on the body
